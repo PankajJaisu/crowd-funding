085199dcb1f51438ba377b955223f6cf0b71ce41
Pankaj Jaiswal <pankaj@Pankajs-MacBook-Air.local>
2023-06-13 00:25:45 +0530

    Added Home Icon

--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -4,9 +4,16 @@ import {Link,useNavigate} from 'react-router-dom';
 import {logo,sun} from '../assets';
 import {navlinks} from '../constants';
 
-const Icon = ()=>{
+const Icon = ({styles,name,imageUrl,isActive,disabled,handleClick})=>(
+<div className={`w-[48px] h-[48px] rounded-[10px] ${isActive && isActive===name && 'bg-[#2c2f32]'} flex justify-center item-center ${!disabled} && 'cursor-pointer'} ${styles}`}>
+    {!isActive?(
+        <img src={imageUrl} alt='fund_logo' className='w-1/2 h-1/2'/>
+    ):(
+        <img src={imageUrl} alt='fund_logo' className={`w-1/2 h-1/2 ${isActive!==name && 'grayscale'}`}/>
+    )}
 
-}
+</div>
+    )
 const SideBar = () => {
     const naviagte = useNavigate();
     const [isActive,setIsActive] = useState('dashboard');
